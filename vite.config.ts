1a408d65747fc1e55a50751584d932410d3a320b
mamelk <mamelkmanama@gmail.com>
2026-06-27 22:25:27 +0200

    Fix: correction syntaxe vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,12 @@ export default defineConfig({
         theme_color: '#f59e0b',
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/logo192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512.png',
+            src: '/logo512.png',
             sizes: '512x512',
             type: 'image/png'
           }
